df6c0df33b57cc636c54b9183ed783c273223c98
Kostia2004 <k.a.kudriavsev@mail.ru>
2022-02-20 01:24:47 +0300

    put the segmentation functions in the class

--- segmentation.py
+++ segmentation.py
@@ -2,15 +2,25 @@ import numpy as np
 import tensorflow as tf
 import cv2
 
-def segmentation(arr):
-    model = tf.keras.models.load_model('C:/Users/sasha/PycharmProjects/Medbot/unet_COVID_lung_model512x512_76_74_AUC98.h5')
-    lung_model = tf.keras.models.load_model('C:/Users/sasha/PycharmProjects/Medbot/lung_unet_model512x512_0222_0217.h5')
-    print(arr.shape)
-    resarr = np.asarray([cv2.resize(arr[:,:,ii], dsize=(512, 512), interpolation=cv2.INTER_AREA).astype('uint8')[..., np.newaxis] for ii in range(arr.shape[2])])
-    ct_slices = resarr.copy()
-    resarr = np.array(resarr)/float(np.max(resarr))
-    lung = lung_model.predict(resarr)
-#    print(*np.unique(resarr))
-    mask = lung<0.01
-    resarr[mask] = 0
-    return model.predict(resarr), resarr, ct_slices 
+class Segmenter():
+    def __init__(self):
+        self.model_path = './unet_COVID_lung_model512x512_76_74_AUC98.h5'
+        self.lung_model_path = './lung_unet_model512x512_0222_0217.h5'
+        self.model = tf.keras.models.load_model(self.model_path)
+        self.lung_model = tf.keras.models.load_model(self.lung_model_path)
+        self.threshold = 0.01
+    
+    def preprocessing(self, ct_scan):
+        arr = ct_scan
+        slicearr = [cv2.resize(arr[:,:,ii], dsize=(512, 512), interpolation=cv2.INTER_AREA).astype('uint8')[..., np.newaxis] for ii in range(arr.shape[2])]
+        preprocess_scan = np.asarray(slicearr)
+        return preprocess_scan
+
+    def segmentation(self, ct_scan):
+        resarr = self.preprocessing(ct_scan)
+        ct_slices = resarr.copy()
+        resarr = np.array(resarr)/float(np.max(resarr))
+        lung = self.lung_model.predict(resarr)
+        mask = lung<0.01
+        resarr[mask] = 0
+        return self.model.predict(resarr), resarr, ct_slices 
